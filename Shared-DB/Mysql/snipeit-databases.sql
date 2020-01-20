266d3b36217bb8adffcf336ae9ba7f2e38d6802e
technicalkiwi <50688197+technicalkiwi@users.noreply.github.com>
2020-01-20 17:14:33 +1300

    remove unused DB line

--- Shared-DB/Mysql/snipeit-databases.sql
+++ Shared-DB/Mysql/snipeit-databases.sql
@@ -1,6 +1,5 @@
 #create databases
 CREATE DATABASE IF NOT EXISTS `Snipe-IT`;
-CREATE DATABASE IF NOT EXISTS `secondary`;
 
 #create root user and grant rights
 CREATE USER 'snipe'@'localhost' IDENTIFIED BY 'Sn1pe';
